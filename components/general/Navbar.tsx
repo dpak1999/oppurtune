bf6ea96ba87730768f986383bfa598dfbdc242a6
Deepak <deepakkumardash313@gmail.com>
2025-01-29 03:36:53 +0530

    Add : light/dark modes

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,11 +1,15 @@
 import { Button } from "../ui/button";
 import Logo from "./Logo";
+import { ThemeToggle } from "./ThemeToggle";
 
 export function Navbar() {
   return (
     <nav className="flex items-center justify-between py-5">
       <Logo />
-      <Button>Log in</Button>
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <Button>Log in</Button>
+      </div>
     </nav>
   );
 }
