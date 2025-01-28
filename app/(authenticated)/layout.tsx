fed8824c7c8b6d1f4a9fd979274d9f619b127397
Deepak <deepakkumardash313@gmail.com>
2025-01-29 03:43:42 +0530

    Add : authenticated route group

---
+++ app/(authenticated)/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { Navbar } from "@/components/general/Navbar";
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 pb-12">
+      <Navbar />
+      {children}
+    </div>
+  );
+};
+
+export default MainLayout;
